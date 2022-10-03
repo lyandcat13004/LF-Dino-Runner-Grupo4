781274a45025febf18efdc5e97bfa28cf88497b5
lyandcat13004 <flores.torrico.lucianae@gmail.com>
2022-10-03 19:33:19 -0400

    CLASS 1 DINOSAUR

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,33 @@
+from email import utils
+from pygame.sprite import Sprite
+
+from utils.constants import RUNNING 
+
+class Dinosaur(Sprite):
+    X_POS = 30
+    Y_POS = 300
+
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.dino_run = True
+        self.dino_step = 0
+
+    def update(self):
+        self.run()
+        if self.dino_step >= 10:
+            self.dino_step = 0
+
+    def draw(self, screen):
+        print(self.dino_step)
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+        
+
+    def run(self):
+        self.image = RUNNING[0] if self.dino_step < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.dino_step += 1
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,5 +1,5 @@
 import pygame
-
+from components.dinosaur import Dinosaur
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
@@ -11,6 +11,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
+        self.dino = Dinosaur()
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
@@ -30,12 +31,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
-
+        self.dino.update()
+        print(self.dino_step)
+        
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.dino.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
