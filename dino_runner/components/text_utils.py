7c90d259706ee6a40043fb436412285567d093f7
lyandcat13004 <flores.torrico.lucianae@gmail.com>
2022-10-05 19:42:17 -0400

    CLASE 3 (errores corregidos)

--- dino_runner/components/text_utils.py
+++ dino_runner/components/text_utils.py
@@ -7,12 +7,14 @@ def get_score_element(points):
     font = pygame.font.Font(FONT_STYLE, 22)
 
     text = font.render('Points:' + str(points), True, black_color)
+    text_rect = text.get_rect()
     text_rect.center = (100, 50)
     return text,text_rect
 
 def get_centered_message(message):
     font = pygame.font.Font(FONT_STYLE, 22)
     text = font.render('Message', True, black_color)
+    text_rect = text.get_rect
     text_rect.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     return text, text_rect
 
